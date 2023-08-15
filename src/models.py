2248293ba0d7d278bab52eb47a3cef0d43b6bda4
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-08-15 18:29:34 +0000

    stars model

--- src/models.py
+++ src/models.py
@@ -7,6 +7,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('characters.id'), unique=True)
+    character = relationship('Characters')
+    planets_id = Column(Integer, ForeignKey('planets.id'), unique=True)
+    planets = relationship('Planets')
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    planet = Column(String(250), unique=True)
+    hair_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)  
+    height = Column(Integer)
+    mass = Column(Integer)  
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, ForeignKey('characters.planet'), primary_key=True)
+    characters = relationship(Characters)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer)
+    gravity = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'), unique=True)
+    favorite = relationship(Favorites)
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
@@ -19,8 +59,8 @@ class Address(Base):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
+    street_name = Column(String(250),nullable=False)
+    street_number = Column(String(250), nullable=False)
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
